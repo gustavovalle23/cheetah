19519a84adf555383e2f986bbe419cbb61a3c2fe
gustavovalle <gustavo.vallerp@hotmail.com>
2024-10-05 23:43:39 +0200

    :sparkles: feat: add intial example of cheetah usage

---
+++ bin/cheetah.dart
@@ -0,0 +1,36 @@
+import 'package:cheetah/cheetah.dart';
+
+void main() async {
+  final app = Cheetah();
+
+  app.use((req, res) async {
+    print('Request: ${req.method} ${req.uri.path}');
+  });
+
+  app.get('/', (req, res) async {
+    res
+      ..write('Welcome to Cheetah!')
+      ..close();
+  });
+
+  app.get('/hello', (req, res) async {
+    res
+      ..write('Hello from Cheetah!')
+      ..close();
+  });
+
+  app.post('/data', (req, res) async {
+    res
+      ..write('Data received via POST!')
+      ..close();
+  });
+
+  app.setErrorHandler((req, res) async {
+    res
+      ..statusCode = 500
+      ..write('Oops! Something went wrong.')
+      ..close();
+  });
+
+  await app.listen();
+}
